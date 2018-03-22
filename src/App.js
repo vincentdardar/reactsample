1a517c80c2ca50a16bda0b89bb7ca43378c99c4c
DESKTOP-RGF5ED5\boss1 <vincent.darmala@gmail.com>
2018-03-22 10:32:25 -0500

    i added panels at the bottom of the menu that will change based on the menu selection

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import React, { Component } from 'react';
 import './App.css';
 import {Header, Container} from "semantic-ui-react";
 
-import TabBarContainer from "./features/TabBarContainer";
+import TabBarContainer from "./features/tabs/TabBarContainer";
+
+const UnitInfo = () => <div>Unit Info content</div>;
+
+const Pilots = () => <div>Pilots content</div>;
+
+const Mechs = () => <div>Mechs content</div>;
+
+const UnitOrganization = () => <div>Unit Organization content</div>;
 
 
 
 class App extends Component {
   render() {
 
+const tabs = [
+    {name : "unitInfo", label : "Unit Info", component : UnitInfo,},
+    {name : "pilots", label : "Pilots", component : Pilots,},
+    {name : "mechs", label : "Mechs", component : Mechs,},
+    {name : "unitOrganization", label : "Unit Organization", component : UnitOrganization}
 
-      const tabs = [
-          {name : "airportInfo", label : "Airport Info"},
-          {name : "pilots", label : "Pilots"},
-          {name : "mechs", label : "Mechs"},
-          {name : "unitOrganization", label : "Unit Organization"}
-      ];
+];
 
 
 
--- src/features/tabs/TabBar.jsx
+++ src/features/tabs/TabBar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import {Menu} from "semantic-ui-react";
-
+import ToggleDisplay from "../../common/components/ToggleDisplay";
 import Tab from "./Tab";
 
 const TabBar = (props) => {
     const {tabs, currentTab, onTabClick, ...otherProps} = props;
+
+
+
 
     const tabItems = tabs.map(tabInfo => {
         const {name, label} = tabInfo;
@@ -20,11 +23,24 @@ const TabBar = (props) => {
         );
     });
 
+
+    const tabPanels = tabs.map(tabInfo => {
+        const {name, component : TabComponent} = tabInfo;
+
+        return (
+            <ToggleDisplay show={name === currentTab} key={name}>
+                <TabComponent />
+            </ToggleDisplay>
+        )
+    })
+
+
     return (
         <div>
             <Menu tabular attached="top" {...otherProps}>
                 {tabItems}
             </Menu>
+            {tabPanels}
         </div>
     )
 }
